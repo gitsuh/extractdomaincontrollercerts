c92aebc316388fa1a1dcb423c6b2096329f30971
gitsuh <21348752+gitsuh@users.noreply.github.com>
2019-10-17 13:21:52 -0400

    Update extractdccerts.sh

--- extractdccerts.sh
+++ extractdccerts.sh
@@ -9,6 +9,6 @@ fi
 
 dig $1 | awk '{print $5}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | while read -r F
 do
-	NAME=$(nslookup $F | grep $1 | awk '{print $4}')
-        openssl s_client -connect "$F":636 2>/dev/null </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > "$NAME".cer
+        NAME=$(nslookup $F | grep $1 | awk '{print $4}')
+        openssl s_client -connect "$F":636 2>/dev/null </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > "$NAME"cer
 done
